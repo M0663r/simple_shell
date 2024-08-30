232f8ef6d9345bed39c4493aa4f451a456e5fed0
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-08-30 14:04:44 -0700

    added entry point for shell

--- exec_cmd.c
+++ exec_cmd.c
@@ -14,58 +14,58 @@
  */
 int exec_cmd(void)
 {
-    char buffer[BUFFER_SIZE];
-    char *command;
-    pid_t pid;
-    int status;
+	char buffer[BUFFER_SIZE];
+	char *command;
+	pid_t pid;
+	int status;
 
-    while (1)
-    {
-        write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
+	while (1)
+	{
+		write(STDOUT_FILENO, PROMPT, strlen(PROMPT));
 
-        if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
-        {
-            if (feof(stdin))
-            {
-                write(STDOUT_FILENO, "\n", 1);
-                exit(EXIT_SUCCESS);
-            }
-            else
-            {
-                perror("fgets");
-                continue;
-            }
-        }
+		if (fgets(buffer, BUFFER_SIZE, stdin) == NULL)
+		{
+			if (feof(stdin))
+			{
+				write(STDOUT_FILENO, "\n", 1);
+				exit(EXIT_SUCCESS);
+			}
+			else
+			{
+				perror("fgets");
+				continue;
+			}
+		}
 
-        buffer[strcspn(buffer, "\n")] = '\0';
+		buffer[strcspn(buffer, "\n")] = '\0';
 
-        if (buffer[0] == '\0')
-        {
-            continue;
-        }
+		if (buffer[0] == '\0')
+		{
+			continue;
+		}
 
-        pid = fork();
-        if (pid < 0)
-        {
-            perror("fork");
-            exit(EXIT_FAILURE);
-        }
+		pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 
-        if (pid == 0)
-        {
-            execlp(buffer, buffer, (char *)NULL);
-            perror(buffer);
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
-            do
-            {
-                waitpid(pid, &status, WUNTRACED);
-            } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        }
-    }
+		if (pid == 0)
+		{
+			execlp(buffer, buffer, (char *)NULL);
+			perror(buffer);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			do
+			{
+				waitpid(pid, &status, WUNTRACED);
+			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		}
+	}
 
-    return 0;
+	return (0);
 }
 
