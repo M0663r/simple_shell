e4b28e2a7f033e0ff7af2202a9452f0a6c1a3080
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-08-30 15:19:26 -0700

    #

--- shell.c
+++ shell.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 
 extern char **environ;
 
 #define MAX_ARGS 64
+#define PATH_MAX_LEN 1024
+
+char *find_command_in_path(char *command);
 
 /**
  * main - entry point for code
@@ -19,19 +25,20 @@ ssize_t nread;
 char *argv[MAX_ARGS];
 char *token;
 int i;
+pid_t child_pid;
 
 while (1)
 {
-printf("#cisfun$ ");
-        
+printf(":) ");
+
 nread = getline(&line, &len, stdin);
-        
+
 if (nread == -1)
 {
 printf("\n");
 break;
 }
-        
+
 line[nread - 1] = '\0';
 
 token = strtok(line, " ");
@@ -41,14 +48,69 @@ argv[i] = token;
 token = strtok(NULL, " ");
 }
 argv[i] = NULL;
-        
-if (execve(argv[0], argv, environ) == -1)
+
+char *command_path = find_command_in_path(argv[0]);
+if (command_path == NULL)
 {
 perror("./shell");
+continue;
+}
+
+child_pid = fork();
+if (child_pid == 0)
+{
+execve(command_path, argv, environ);
+perror("./shell");
+exit(EXIT_FAILURE);
+}
+else if (child_pid < 0)
+{
+perror("fork");
+}
+else
+{
+wait(NULL);
+}
+
+if (command_path != argv[0])
+{
+free(command_path);
 }
 }
-    
+
 free(line);
-return 0;
+return (0);
+}
+
+char *find_command_in_path(char *command)
+{
+char *path = getenv("PATH");
+char *path_dup = strdup(path);
+char *dir = strtok(path_dup, ":");
+struct stat st;
+char full_path[PATH_MAX_LEN];
+
+if (strchr(command, '/') != NULL)
+{
+if (stat(command, &st) == 0 && (st.st_mode & S_IXUSR))
+{
+free(path_dup);
+return (command);
+}
+}
+
+while (dir != NULL)
+{
+snprintf(full_path, PATH_MAX_LEN, "%s/%s", dir, command);
+if (stat(full_path, &st) == 0 && (st.st_mode & S_IXUSR))
+{
+free(path_dup);
+return (strdup(full_path));
+}
+dir = strtok(NULL, ":");
+}
+
+free(path_dup);
+return (NULL);
 }
 
