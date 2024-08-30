76f2a50b6ec84918a13885483eba81b39ed6764a
M0663r <s-ahmad.el-abassy@zewailcity.edu.eg>
2024-08-30 14:26:41 -0700

    #

--- shell.c
+++ shell.c
@@ -1,16 +1,41 @@
-#include "shell.h"
-/**
- * main - entry point of program
- *
- *
- *Return:0
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char **environ;
+
 int main(void)
 {
-const char text[] = "Simple Shell project is quite hard!";
-write_str(text);
-
-
-
+char *line = NULL;
+size_t len = 0;
+ssize_t nread;
+char *argv[2];
+    
+while (1)
+{
+printf("#cisfun$ ");
+        
+nread = getline(&line, &len, stdin);
+        
+if (nread == -1)
+{
+printf("\n");
+break;
+}
+        
+line[nread - 1] = '\0';
+        
+argv[0] = line;
+argv[1] = NULL;
+        
+if (execve(argv[0], argv, environ) == -1)
+{
+perror("./shell");
+}
+}
+    
+free(line);
 return (0);
 }
+
